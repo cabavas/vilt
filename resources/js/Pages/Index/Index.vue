0099cff9e7b86525635fc2833aca8f881b67c9ac
cabavas <cabavas@gmail.com>
2024-11-12 13:39:57 -0300

    Aula 24

---
+++ resources/js/Pages/Index/Index.vue
@@ -0,0 +1,8 @@
+<template>
+    <div>Index Vue</div>
+    <Link href="/hello">Show Page</Link>
+</template>
+
+<script setup>
+import { Link } from "@inertiajs/vue3";
+</script>
---
+++ resources/js/Pages/Index/Show.vue
@@ -0,0 +1,8 @@
+<template>
+    <div>Show Vue</div>
+    <Link href="/">Show Page</Link>
+</template>
+
+<script setup>
+import { Link } from "@inertiajs/vue3";
+</script>
