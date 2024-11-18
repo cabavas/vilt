9aa75cc8a936571142a59bcadb86f1f824a84719
cabavas <cabavas@gmail.com>
2024-11-18 12:37:43 -0300

    Aula 49

--- resources/js/Pages/Layouts/MainLayout.vue
+++ resources/js/Pages/Layouts/MainLayout.vue
@@ -1,14 +1,30 @@
 <template>
     <Link href="/">Main Page</Link>&nbsp;
     <Link href="/hello">Show Page</Link>
-    <div>The page with time {{ timer }}</div>
-    <slot/>
+    <!-- <div>The page with time {{ timer }}</div> -->
+    <div v-if="flashSuccess" class="success">
+        {{ flashSuccess }}
+    </div>
+    <slot />
 </template>
 
 <script setup>
-import { Link } from '@inertiajs/vue3';
-import { ref } from 'vue';
+import { computed } from 'vue';
+import { Link, usePage } from '@inertiajs/vue3';
+// page.props.value.flash.success
+const page = usePage()
+const flashSuccess = computed(
+    () => page.props.value?.flash?.success
+)
+// import { ref } from 'vue';
 
-const timer = ref(0);
-setInterval(() => timer.value++, 1000);
+// const timer = ref(0);
+// setInterval(() => timer.value++, 1000);
 </script>
+
+<style scoped>
+.success {
+    background-color: green;
+    color: white
+}
+</style>
