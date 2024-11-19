6e8d76e46c47a8782a6af1206d0b66e845dd9219
cabavas <cabavas@gmail.com>
2024-11-19 10:15:45 -0300

    Aula 60

--- resources/js/Pages/Layouts/MainLayout.vue
+++ resources/js/Pages/Layouts/MainLayout.vue
@@ -1,10 +1,29 @@
 <template>
-    <Link :href="route('listing.index')">Listings</Link>&nbsp;
-    <Link :href="route('listing.create')">New Listing</Link>
-    <div v-if="flashSuccess" class="success">
-        {{ flashSuccess }}
-    </div>
-    <slot />
+
+    <header class="border-b border-gray-200 bg-white dark:border-gray-700 dark:bg-gray-900 w-full">
+        <div class="container mx-auto">
+            <nav class="p-4 flex items-center justify-between">
+                <div class="text-lg font-medium">
+                    <Link :href="route('listing.index')">Listings</Link>&nbsp;
+                </div>
+                <div class="text-xl text-indigo-600 dark:text-indigo-300 font-bold text-center">
+                    <Link :href="route('listing.index')">Larazillow</Link>
+                </div>
+                <div>
+                    <Link :href="route('listing.create')"
+                        class="bg-indigo-600 hover:bg-indigo-500 text-white font-medium p-2 rounded-md">+ New Listing
+                    </Link>
+                </div>
+            </nav>
+        </div>
+    </header>
+
+    <main class="container mx-auto p-4">
+        <div v-if="flashSuccess" class="mb-4 border rounded-md shadow-sm border-green-200 dark:border-green-800 bg-green-50 dark:bg-green-900 p-2">
+            {{ flashSuccess }}
+        </div>
+        <slot />
+    </main>
 </template>
 
 <script setup>
@@ -16,10 +35,3 @@ const flashSuccess = computed(
     () => page.props.flash.success
 )
 </script>
-
-<style scoped>
-.success {
-    background-color: green;
-    color: white
-}
-</style>
--- resources/js/Pages/Listing/Show.vue
+++ resources/js/Pages/Listing/Show.vue
@@ -1,13 +1,27 @@
 <template>
     <div>
-        <ListingAddress :listing="listing" />
+      <div class="flex flex-col-reverse md:grid md:grid-cols-12 gap-4">
+        <Box class="md:col-span-7 flex items-center">
+          <div class="w-full">
+            <div class="flex items-center gap-2">
+              <Price :price="listing.price" class="text-2xl font-bold" />
+            </div>
+            <ListingSpace :listing="listing" class="text-lg" />
+            <ListingAddress :listing="listing" class="text-gray-500" />
+          </div>
+        </Box>
+      </div>
     </div>
-</template>
-
-<script setup>
-import ListingAddress from '../Components/ListingAddress.vue';
-
-defineProps({
+  </template>
+  
+  <script setup>
+  import ListingAddress from '@/Pages/Components/ListingAddress.vue'
+  import ListingSpace from '@/Pages/Components/ListingSpace.vue'
+  import Price from '@/Pages/Components/Price.vue'
+  import Box from '@/Pages/Components/UI/Box.vue'
+  
+  defineProps({
     listing: Object,
-})
-</script>
+  })
+  </script>
+  
