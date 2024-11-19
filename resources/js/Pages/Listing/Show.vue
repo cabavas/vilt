4e74975b3aff628abb1e7725b3429b78dadb11fd
cabavas <cabavas@gmail.com>
2024-11-19 12:16:47 -0300

    Aula 69

--- resources/js/Pages/Listing/Show.vue
+++ resources/js/Pages/Listing/Show.vue
@@ -19,12 +19,18 @@
         </template>
         <div>
           <label class="label">Interest rate ({{ interestRate }}%)</label>
-          <input v-model.number="interestRate" type="range" min="0.1" max="30" step="0.1"
-            class="w-full h-4 bg-gray-200 rounded-lg appearance-none cursor-pointer dark:bg-gray-700" />
+          <input
+            v-model.number="interestRate"
+            type="range" min="0.1" max="30" step="0.1"
+            class="w-full h-4 bg-gray-200 rounded-lg appearance-none cursor-pointer dark:bg-gray-700"
+          />
 
           <label class="label">Duration ({{ duration }} years)</label>
-          <input v-model.number="duration" type="range" min="3" max="35" step="1"
-            class="w-full h-4 bg-gray-200 rounded-lg appearance-none cursor-pointer dark:bg-gray-700" />
+          <input
+            v-model.number="duration"
+            type="range" min="3" max="35" step="1"
+            class="w-full h-4 bg-gray-200 rounded-lg appearance-none cursor-pointer dark:bg-gray-700"
+          />
 
           <div class="text-gray-600 dark:text-gray-300 mt-2">
             <div class="text-gray-400">Your monthly payment</div>
@@ -37,13 +43,14 @@
 </template>
 
 <script setup>
-import ListingAddress from '../Components/ListingAddress.vue'
-import ListingSpace from '../Components/ListingSpace.vue'
-import Price from '../Components/Price.vue'
-import Box from '../Components/UI/Box.vue'
+import ListingAddress from '@/Components/ListingAddress.vue'
+import ListingSpace from '@/Components/ListingSpace.vue'
+import Price from '@/Components/Price.vue'
+import Box from '@/Components/UI/Box.vue'
 
-import { ref } from 'vue'
-import { useMonthlyPayment } from '@/Composables/useMonthlyPayment'
+import {ref} from 'vue'
+
+import {useMonthlyPayment} from '@/Composables/useMonthlyPayment'
 
 const interestRate = ref(2.5)
 const duration = ref(25)
@@ -52,5 +59,7 @@ const props = defineProps({
   listing: Object,
 })
 
-const { monthlyPayment } = useMonthlyPayment(props.listing.price, interestRate, duration)
+const { monthlyPayment } = useMonthlyPayment(
+  props.listing.price, interestRate, duration,
+)
 </script>
